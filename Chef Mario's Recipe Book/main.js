a0cf412c48e723f9c202410e887959361cbf2108
aghyadghziel <EDGXaghyad@gmail.com>
2023-04-20 10:05:05 +0300

    New Projects

---
+++ Chef Mario's Recipe Book/main.js
@@ -0,0 +1,38 @@
+/* Chef Mario's Recipe Book 
+Chef Mario was in the middle of writing his cookbook masterpiece
+when he spilled coffee on his keyboard! Now all his recipes have repeat
+ingredients.
+
+Help save Chef Mario's cookbook by writing a function that takes in an array 
+and returns a new array with all the duplicates removed. 
+
+Example input: ["🌈 rainbow", "🦄 unicorn", "🍭 lollipops", "🦄 unicorn", "🍭 lollipops"];
+Example output: ["🌈 rainbow", "🦄 unicorn", "🍭 lollipops"];
+*/
+
+const eggScrambleRecipe = [
+  "🥓 bacon",
+  "🥓 bacon",
+  "🍳 eggs",
+  "🫑 green peppers",
+  "🧀 cheese",
+  "🌶️ hot sauce",
+  "🥓 bacon",
+  "🥦 broccoli",
+  "🧀 cheese",
+  "🥦 broccoli",
+  "🌶️ hot sauce",
+];
+
+function removeDupesFromArray(arr) {
+  const uniqueItems = [];
+
+  arr.forEach((item) => {
+    if (!uniqueItems.includes(item)) {
+      uniqueItems.push(item);
+    }
+  });
+  return uniqueItems;
+}
+
+console.log(removeDupesFromArray(eggScrambleRecipe));
